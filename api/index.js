18045f6c8d0d93b0c4bf565b82c3627fbd245a63
rukivboki <89135243+rukivboki@users.noreply.github.com>
2025-04-08 12:06:18 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,11 +1,10 @@
-
 export default async function handler(req, res) {
   const token = process.env.VK_TOKEN;
   const groupId = '116712716';
-  const count = 100;
+  const offset = req.query.offset || 0;
+  const count = req.query.count || 5;
 
-  const url = `https://api.vk.com/method/wall.get?owner_id=-${groupId}&count=${count}&access_token=${token}&v=5.199`;
-
+  const url = `https://api.vk.com/method/wall.get?owner_id=-${groupId}&offset=${offset}&count=${count}&access_token=${token}&v=5.199`;
   const response = await fetch(url);
   const data = await response.json();
 
