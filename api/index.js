5b5d05eb15829883a8f2139b23ff29aebc384ad9
rukivboki <89135243+rukivboki@users.noreply.github.com>
2025-04-08 11:17:16 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 
 export default async function handler(req, res) {
-  const token = 'vk1.a.xfYvgk5pAE_zv5GxMGI7luvkWCMd2wPIjcvmSWoSH3iGi0wPqJjzOUSQSjqxs9-Rf33rwoSgeyoQcId1zct5y3ilxdIZv_tkxW8L1sgU6O3bYAAyZDEeK_5K1EHz4cb8ZGXNJlUmbCvK0TIN75j2gRjhZT7HUXJbTWKPDx8RH-yO2ob-EmJyiLyS3YL53uwIwNkH30_mi_Hwww5RVDp6xA';
+  const token = process.env.VK_TOKEN;
   const groupId = '116712716';
   const count = 5;
 
