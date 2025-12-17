c3090b3c79d1c02a24893764f8f154fcc311f0ef
rukivboki <89135243+rukivboki@users.noreply.github.com>
2025-12-17 11:33:11 +0300

    Update index.js
    new token

--- api/index.js
+++ api/index.js
@@ -1,13 +1,15 @@
+
 export default async function handler(req, res) {
-  const token = process.env.VK_TOKEN;
   const groupId = '116712716';
   const offset = req.query.offset || 0;
   const count = req.query.count || 5;
 
-  const url = `https://api.vk.com/method/wall.get?owner_id=-${groupId}&offset=${offset}&count=${count}&access_token=${token}&v=5.199`;
+  const url = `https://api.vk.com/method/wall.get?owner_id=-${groupId}&offset=${offset}&count=${count}&v=5.199`;
+
   const response = await fetch(url);
   const data = await response.json();
 
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(200).json(data);
 }
+
